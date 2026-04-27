8a427c084bc71e9b176cc600f9355c2a35148ac6
Medora <medoragomes1402@gmail.com>
2026-04-27 19:04:25 +0530

    fixed end point in resumeAIService.js

--- career_tracker/client/src/services/resumeAIService.js
+++ career_tracker/client/src/services/resumeAIService.js
@@ -1,7 +1,7 @@
 import API from "./api";
 
 export const analyzeResumeAI = async (text, role) => {
-  const res = await API.post("/api/ai/analyze-ai", {
+  const res = await API.post("/ai/analyze-ai", {
     text,
     role,
   });
